8572fb9f4075029f8a6aa3ed160328c14c8fc58c
David Becher <david@skookum.com>
2012-07-13 17:18:32 -0400

    change jade file structure and add sidebar with scroll spy stuff

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-// Code for documentation goes here
+$(function() {
+  if($('#sidebar').length > 0) {
+    var $window = $(window);
+    var $sidebar = $('#sidebar');
+    var sidebar_top = $sidebar.offset().top - 16;
+    function positionSidebar() {
+      $sidebar.toggleClass('fixed', $window.scrollTop() > sidebar_top);
+    }
+    $(window).on('scroll', positionSidebar);
+    positionSidebar();
+  }
+});
